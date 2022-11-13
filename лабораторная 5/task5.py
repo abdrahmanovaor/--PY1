ebf707809dfcad8e573f3a1d4bff860d2bbd1e04
abdrahmanovaor <115338405+abdrahmanovaor@users.noreply.github.com>
2022-11-13 23:11:09 +0300

    Update task5.py

--- лабораторная 5/task5.py
+++ лабораторная 5/task5.py
@@ -1,5 +1,10 @@
+import random
+from random import sample
 def get_random_password() -> str:
-    ...  # TODO написать функцию генерации случайных паролей
-
+   symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    password = ''
+    i = random.sample(symbols, 8)
+    password += str (i)
+    return(password)
 
 print(get_random_password())
